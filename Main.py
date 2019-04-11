d06cb272ff5203640cbd9a45ebcf9697651dcf31
Trump <g1017557273@163.com>
2019-04-11 23:27:15 +0800

    !!!

--- Main.py
+++ Main.py
@@ -1,17 +1,10 @@
 from Node import *
-import numpy as np
 import ShortestPath
 
 lst_node = []
 MAX_DIST = 100000
-#dist = np.full((1000, 1000), MAX_DIST, dtype=float)
-dist = [[MAX_DIST] * 1000] * 1000
+dis = [([MAX_DIST] * 1000) for i in range(1000)]
 
-
-def func():
-    x1 = node(3,2)
-    x2 = node(2,3)
-    print(x1.Calc_Dist(x2))
 
 def Data_Init():
     f1 = open("city.txt", encoding="utf-8")
@@ -28,16 +21,10 @@ def Data_Init():
             continue
         lst_node[a].lst_link.append(b)
         lst_node[b].lst_link.append(a)
-        dist[a][b] = lst_node[a].Calc_Dist(lst_node[b])
-        dist[b][a] = lst_node[a].Calc_Dist(lst_node[b])
+        dis[a][b] = lst_node[a].Calc_Dist(lst_node[b])
+        dis[b][a] = lst_node[a].Calc_Dist(lst_node[b])
 
 if __name__ == '__main__':
-    #func()
-    for i in range(1000):
-        print(dist[0][i])
     Data_Init()
-    '''for each in lst_node:
-        print(each.lst_link)'''
-    for i in range(1000):
-        print(dist[0][i])
-    ShortestPath.ShortestPath_Dijkstra(dist, 0, 100)
+    answer = ShortestPath.ShortestPath_Dijkstra(dis, 0, 345)
+    print(answer)
